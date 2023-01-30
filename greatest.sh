ff391d35cdbda13b6b8d4f540f25bd24b84d6f05
it's me madhan <112246308+madhansaireddy@users.noreply.github.com>
2023-01-30 09:33:34 +0530

    Create greatest.sh

---
+++ greatest.sh
@@ -0,0 +1,13 @@
+echo "enter the three numbers"
+read a
+read b
+read c
+if [ $a -gt $b ] && [ $a -gt $c ]
+then
+echo $a "is the greatest number"
+elif [ $b -gt $a ] && [ $b -gt $c ]
+then
+echo $b "is the greatest number"
+else
+echo $c "is the greatest number"
+fi 
